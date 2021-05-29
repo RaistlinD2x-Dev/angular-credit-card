b46a3d3314aaeb9702938edb617a697ecdb5c338
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-29 11:08:25 -0500

    disabled letters

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -2,10 +2,12 @@ import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl{
     setValue(value: string, options: any) {
+        // doesn't allow letters
         if (value.match(/[^0-9|\/]/gi)) {
             super.setValue(this.value, {...options, emitModelToViewChange: true})
             return;
         }
+        // add a slash after 2 numbers have been added
         if(value.length === 2){
             super.setValue(value + '/', {...options, emitModelToViewChange: true});
             return;
